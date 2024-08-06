40f83aebe55afad7ad865d62bffa362021c6bf82
Naved khan <nomanali50997@gmail.com>
2024-08-06 18:13:24 +0530

    first commit

---
+++ naved/naved/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+def home(request):
+    data = {
+        'tittle':'Home Page',
+        'bdata':'Welcome to real world',
+        'cdata':["Python","mern","java"],
+        'numbers':[10,20,30,40,50,60,70],
+        'student_details':[
+            {'name':'jijo','phone':8054682324},
+            {'name':'kiko','phone':8054546832}
+        ]
+    }
+    return render(request,"index.html",data)
+
+def aboutus(request):
+    return HttpResponse(" Welcome to Naved And Django to About us page ")
+
+def course(request):
+    return HttpResponse("<b> Hi am course and this is cource page </b>")
+
+def courseDetail(request,courseid):
+    return HttpResponse(courseid)
